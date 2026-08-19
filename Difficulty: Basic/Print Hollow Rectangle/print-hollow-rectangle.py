f71bec160b924b1685538be9fd0ba3b369b873ac
Kamalakara M Marathi <kamalakaramarathi13@gmail.com>
2026-08-20 01:33:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Hollow Rectangle/print-hollow-rectangle.py
@@ -0,0 +1,11 @@
+n = int(input())
+m = int(input())
+
+# code here
+for i in range(n):
+    for j in range(m):
+        if i==0 or i == n-1 or j == 0 or j == m-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
