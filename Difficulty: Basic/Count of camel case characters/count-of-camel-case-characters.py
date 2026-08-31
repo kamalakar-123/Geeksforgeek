4ee4779276f40454fbe987d56821a2e01f43ba14
Kamalakara M Marathi <kamalakaramarathi13@gmail.com>
2026-08-31 20:32:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count of camel case characters/count-of-camel-case-characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countCamelCase (self,s):
+        # your code here
+        count=0
+        for ch in s:
+            if ch.isupper():
+                count+=1
+        return count
